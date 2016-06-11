a0372037a04cbed586a95cb5997237318931d7b0
kelishrestha <kylesth@gmail.com>
2016-06-11 11:39:22 +0545

    Add route for listing chapters of a manga.

--- app.rb
+++ app.rb
@@ -19,3 +19,23 @@ post '/search/sites/' do
 
 	haml :_list
 end
+
+# List manga chapters
+post '/manga/:name/chapters' do
+	name = params['name']
+	page_link = params['page_link']
+	mechanize = Mechanize.new
+	page = mechanize.get(page_link)
+	chapters = page.search('.tips')
+	@chapters = chapters.inject([]) do |lists, page_link|
+		title_hash = {}
+		title_hash['title'] = page_link.text.strip
+		title_hash['link'] = page_link.attribute('href').value
+
+		lists << title_hash
+	end
+	# Search for image link and traverse with next image links
+	chapter_page = mechanize.get(chapter)
+	image = chapter_page.search('.image')
+	next_image_page = chapter_page.link_with(text: 'next page').click
+end
